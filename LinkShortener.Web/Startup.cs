18877cb854be4056450988837e073511a6a9ccd7
Stuart Carruthers <stu@ironiclensflare.com>
2017-06-10 16:12:56 +0100

    Add project files.

---
+++ LinkShortener.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LinkShortener.Web.Startup))]
+namespace LinkShortener.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
